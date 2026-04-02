65b5147c96e20ce8cead63f369e4f0b992a69e3d
João Serafim <168292874+JoaoLucasSerafim@users.noreply.github.com>
2026-04-01 22:01:32 -0300

    Added the pages

--- Login/urls.py
+++ Login/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
-    path("", views.login_home),
-    path("login.agro", views.login_agricultor)
+    path("", views.login_home, name="login_home"),
+    path("login_agro", include([
+        path ("", views.login_agricultor, name="login_agricultor"),
+        path("criar_agro", views.criar_agricultor, name="criar_agricultor")])),
+    path("login_tec", views.login_tecnico, name="login_tecnico"),
 ]
--- Login/views.py
+++ Login/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 def login_home(request):
     return render(request, "login/login_page.html")
 def login_agricultor(request):
-    return HttpResponse("Adicione aqui seu login, ou crie uma conta")
+    return render(request, "login/login_agro.html")
+def login_tecnico(request):
+    return render(request, "login/login_tec.html")
+def criar_agricultor(request):
+    return render(request, "login/criar_agro.html")
