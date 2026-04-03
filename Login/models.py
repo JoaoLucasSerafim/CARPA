45c9a7cd9a98062311d23df281296f00bc71823d
João Serafim <168292874+JoaoLucasSerafim@users.noreply.github.com>
2026-04-03 11:58:59 -0300

    added models

--- Login/models.py
+++ Login/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class CriarModel(models.Model):
+    class agriinfo(models.Model):
+        nome = models.CharField(max_length=100)
+        cpf = models.CharField(max_length=14)
+        rg = models.CharField(max_length=12)
+    class geospace(models.Model):
+        cep = models.CharField(max_length=9)
+        docposse = models.BooleanField()
+        vizinhos = models.CharField()
+        Total_ha = models.FloatField()
+        property_name = models.CharField()
+        reservalegal_ha = models.FloatField()
+        app_ha = models.FloatField()
+        uso_ha = models.FloatField()
+        consolidada_ha = models.FloatField()
+        corposHidricos = models.IntegerField()
+        nascentes = models.IntegerField()
